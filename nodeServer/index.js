fe28ae410974d44c9aeaf22333475ce5ad90c16f
rahul <rahuldusa37@gmail.com>
2024-02-17 12:47:27 +0530

    first version

---
+++ nodeServer/index.js
@@ -0,0 +1,30 @@
+const io = require('socket.io')({
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST"]
+    }
+  });
+  
+  io.listen(8000);
+  
+
+const users={};
+
+io.on('connection',socket=>{  //listens to the users joining to the chat
+    socket.on('new-user-joined',name=>{  //it deals with the connections
+            console.log("new user",name);
+            users[socket.id]= name ;
+            socket.broadcast.emit('user-joined',name);//it notifys the other users that new user jooined  
+                
+    });
+    socket.on('send',message=>{ //when anyone sends message this works we can use any name instead of sesnd
+        socket.broadcast.emit('receive',{message:message,name:users[socket.id]})
+    });
+
+    socket.on('disconnect',message=>{ //when anyone sends message this works we can use any name instead of sesnd
+        socket.broadcast.emit('left',users[socket.id]);
+        delete users[socket.id];
+    });
+})
+
+
